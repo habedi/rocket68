b6865b0156e1a8a940eb7f8a0b16f26cf2b2342f
Hassan Abedi <hassan.abedi.t@gmail.com>
2026-03-02 10:21:37 +0100

    The base commit

--- include/rocket68.h
+++ include/rocket68.h
@@ -14,7 +14,7 @@ extern "C" {
 #include "m68k.h"
 
 /** @brief Rocket 68 semantic version string. */
-#define ROCKET68_VERSION_STR "0.1.0-alpha.3"
+#define ROCKET68_VERSION_STR "0.1.0-alpha.4"
 
 #ifdef __cplusplus
 }
