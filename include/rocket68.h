3708130f3db61b802da5ef0e9bc46050d335dc83
Hassan Abedi <hassan.abedi.t@gmail.com>
2026-03-04 08:57:58 +0100

    Fix the memory bug (#10)

--- include/rocket68.h
+++ include/rocket68.h
@@ -14,7 +14,7 @@ extern "C" {
 #include "m68k.h"
 
 /** @brief Rocket 68 semantic version string. */
-#define ROCKET68_VERSION_STR "0.1.0-alpha.4"
+#define ROCKET68_VERSION_STR "0.1.0-alpha.5"
 
 #ifdef __cplusplus
 }
