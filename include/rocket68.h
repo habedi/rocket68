db2b70dff44ef29043916365760a6c3522190fd5
Hassan Abedi <hassan.abedi.t@gmail.com>
2026-08-08 12:23:45 +0200

    Update version to `0.3.0` (due to API changes)

--- include/rocket68.h
+++ include/rocket68.h
@@ -14,7 +14,7 @@ extern "C" {
 #include "m68k.h"
 
 /** @brief Rocket 68 semantic version string. */
-#define ROCKET68_VERSION_STR "0.2.4"
+#define ROCKET68_VERSION_STR "0.3.0"
 
 #ifdef __cplusplus
 }
